c5703e949572d5728b9fbe5d46222fac1c2ddba3
sgwag6799 <sgwag6799@gmail.com>
2026-05-19 15:31:57 +0900

    feat: 공동 컴포넌트 input 구현

---
+++ src/component/input/Input.jsx
@@ -0,0 +1,36 @@
+import styles from './Input.module.css';
+
+export default function Input({
+  label = 'input',
+  placeholder = '소개 멘트 작성해주세요 ',
+  value,
+  onChange,
+  maxLength = 300,
+  textarea = false,
+}) {
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.label}>{label}</label>
+
+      {textarea ? (
+        <textarea
+          className={styles.input}
+          style={{ minHeight: '98px' }}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          maxLength={maxLength}
+        />
+      ) : (
+        <input
+          className={styles.input}
+          style={{ height: '48px' }}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          maxLength={maxLength}
+        />
+      )}
+    </div>
+  );
+}
