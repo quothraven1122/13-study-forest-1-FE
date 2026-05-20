7b9d8863cd5e39e8679be9b2b8178d4b80007e44
apple <apple@appleui-MacBookAir.local>
2026-05-20 18:07:32 +0900

    feat: 공용 컴포넌트 modal1 작업 수정 완료

--- src/component/Modal1/Modal1.jsx
+++ src/component/Modal1/Modal1.jsx
@@ -1,128 +1,31 @@
-// import ic_trash from '../../asset/icon/ic_trash.svg';
-// import ic_plus from '../../asset/icon/ic_plus.svg';
-// import styles from './Modal1.module.css';
-// import { useState } from 'react';
-// export default function Modal1({ habits, onSave, onCancel }) {
-//   const [localHabits, setLocalHabits] = useState(habits.map((h) => ({ ...h })));
-//   // 수정
-//   const onChangeHabit = (id, value) => {
-//     setLocalHabits((prev) =>
-//       prev.map((h) => (h.id === id ? { ...h, name: value } : h))
-//     );
-//   };
-
-//   // 삭제
-//   const onDeleteHabit = (id) => {
-//     setLocalHabits((prev) => prev.filter((h) => h.id !== id));
-//   };
-
-//   // 추가
-//   const onAddHabit = () => {
-//     setLocalHabits((prev) => [...prev, { id: Date.now(), name: '' }]);
-//   };
-//   return (
-//     <div className={styles.modalOverlay}>
-//       <div className={styles.modal}>
-//         <p className={styles.title}>습관 목록</p>
-//         <div className={styles.habitListBox}>
-//           {localHabits.map((habit) => (
-//             <div className={styles.habitBox} key={habit.id}>
-//               <input
-//                 type='text'
-//                 className={styles.habit}
-//                 value={habit.name}
-//                 onChange={(e) => onChangeHabit(habit.id, e.target.value)}
-//               />
-
-//               <button
-//                 className={styles.deleteBtn}
-//                 onClick={() => onDeleteHabit(habit.id)}
-//               >
-//                 <img src={ic_trash} alt='습관 삭제' />
-//               </button>
-//             </div>
-//           ))}
-//         </div>
-
-//         <button className={styles.addBtn} type='button' onClick={onAddHabit}>
-//           <img src={ic_plus} alt='습관 추가' />
-//         </button>
-
-//         <div className={styles.btnBox}>
-//           {/* 나중에 공용 버튼 컴포넌트로 변경 */}
-//           <button className={styles.cancelBtn} onClick={onCancel}>
-//             취소
-//           </button>
-//           <button
-//             className={styles.saveBtn}
-//             onClick={() => onSave(localHabits)}
-//           >
-//             수정완료
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-//============================UI 확인용 코드===================================
 import ic_trash from '../../asset/icon/ic_trash.svg';
 import ic_plus from '../../asset/icon/ic_plus.svg';
 import styles from './Modal1.module.css';
 import { useState } from 'react';
-export default function Modal1({ onSave, onCancel }) {
-
-  const [habits, setHabits] = useState([
-    {
-      id: 1,
-      name: '오늘의 습관',
-    },
-    {
-      id: 2,
-      name: '오늘의 습관',
-    },
-    {
-      id: 3,
-      name: '오늘의 습관',
-    },
-    {
-      id: 4,
-      name: '오늘의 습관',
-    },
-    {
-      id: 5,
-      name: '오늘의 습관',
-    },
-    {
-      id: 6,
-      name: '오늘의 습관',
-    },
-  ]);
-
+export default function Modal1({ habits, onSave, onCancel }) {
+  const [localHabits, setLocalHabits] = useState(habits.map((h) => ({ ...h })));
+  // 수정
   const onChangeHabit = (id, value) => {
-    const updateHabits = habits.map((habit) =>
-      habit.id === id ? { ...habit, name: value } : habit
+    setLocalHabits((prev) =>
+      prev.map((h) => (h.id === id ? { ...h, name: value } : h))
     );
-    setHabits(updateHabits);
   };
 
+  // 삭제
   const onDeleteHabit = (id) => {
-    const filteredHabits = habits.filter((habit) => habit.id !== id);
-    setHabits(filteredHabits);
+    setLocalHabits((prev) => prev.filter((h) => h.id !== id));
   };
 
+  // 추가
   const onAddHabit = () => {
-    const newHabit = {
-      id: Date.now(),
-      name: '',
-    };
-    setHabits([...habits, newHabit]);
+    setLocalHabits((prev) => [...prev, { id: Date.now(), name: '' }]);
   };
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modal}>
         <p className={styles.title}>습관 목록</p>
         <div className={styles.habitListBox}>
-          {habits.map((habit) => (
+          {localHabits.map((habit) => (
             <div className={styles.habitBox} key={habit.id}>
               <input
                 type='text'
@@ -140,6 +43,7 @@ export default function Modal1({ onSave, onCancel }) {
             </div>
           ))}
         </div>
+
         <button className={styles.addBtn} type='button' onClick={onAddHabit}>
           <img src={ic_plus} alt='습관 추가' />
         </button>
@@ -149,7 +53,10 @@ export default function Modal1({ onSave, onCancel }) {
           <button className={styles.cancelBtn} onClick={onCancel}>
             취소
           </button>
-          <button className={styles.saveBtn} onClick={onSave}>
+          <button
+            className={styles.saveBtn}
+            onClick={() => onSave(localHabits)}
+          >
             수정완료
           </button>
         </div>
@@ -157,3 +64,96 @@ export default function Modal1({ onSave, onCancel }) {
     </div>
   );
 }
+//============================UI 확인용 코드===================================
+// import ic_trash from '../../asset/icon/ic_trash.svg';
+// import ic_plus from '../../asset/icon/ic_plus.svg';
+// import styles from './Modal1.module.css';
+// import { useState } from 'react';
+// export default function Modal1({ onSave, onCancel }) {
+
+//   const [habits, setHabits] = useState([
+//     {
+//       id: 1,
+//       name: '오늘의 습관',
+//     },
+//     {
+//       id: 2,
+//       name: '오늘의 습관',
+//     },
+//     {
+//       id: 3,
+//       name: '오늘의 습관',
+//     },
+//     {
+//       id: 4,
+//       name: '오늘의 습관',
+//     },
+//     {
+//       id: 5,
+//       name: '오늘의 습관',
+//     },
+//     {
+//       id: 6,
+//       name: '오늘의 습관',
+//     },
+//   ]);
+
+//   const onChangeHabit = (id, value) => {
+//     const updateHabits = habits.map((habit) =>
+//       habit.id === id ? { ...habit, name: value } : habit
+//     );
+//     setHabits(updateHabits);
+//   };
+
+//   const onDeleteHabit = (id) => {
+//     const filteredHabits = habits.filter((habit) => habit.id !== id);
+//     setHabits(filteredHabits);
+//   };
+
+//   const onAddHabit = () => {
+//     const newHabit = {
+//       id: Date.now(),
+//       name: '',
+//     };
+//     setHabits([...habits, newHabit]);
+//   };
+//   return (
+//     <div className={styles.modalOverlay}>
+//       <div className={styles.modal}>
+//         <p className={styles.title}>습관 목록</p>
+//         <div className={styles.habitListBox}>
+//           {habits.map((habit) => (
+//             <div className={styles.habitBox} key={habit.id}>
+//               <input
+//                 type='text'
+//                 className={styles.habit}
+//                 value={habit.name}
+//                 onChange={(e) => onChangeHabit(habit.id, e.target.value)}
+//               />
+
+//               <button
+//                 className={styles.deleteBtn}
+//                 onClick={() => onDeleteHabit(habit.id)}
+//               >
+//                 <img src={ic_trash} alt='습관 삭제' />
+//               </button>
+//             </div>
+//           ))}
+//         </div>
+//         <button className={styles.addBtn} type='button' onClick={onAddHabit}>
+//           <img src={ic_plus} alt='습관 추가' />
+//         </button>
+
+//         <div className={styles.btnBox}>
+//           {/* 나중에 공용 버튼 컴포넌트로 변경 */}
+//           <button className={styles.cancelBtn} onClick={onCancel}>
+//             취소
+//           </button>
+//           <button className={styles.saveBtn} onClick={onSave}>
+//             수정완료
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
