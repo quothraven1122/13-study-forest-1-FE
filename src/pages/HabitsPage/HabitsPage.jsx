fc34de8249524b32db2c7466d5f38525170ae959
seonghwi-ha <digh7515@gmail.com>
2026-05-22 14:11:44 +0900

    feat: 오늘의 습관 퍼블리싱 및 Chip 컴포넌트 구현

--- src/pages/HabitsPage/HabitsPage.jsx
+++ src/pages/HabitsPage/HabitsPage.jsx
@@ -1,5 +1,67 @@
+import { useState } from 'react';
+import './HabitsPage.css';
+import Chip from '../../components/Chip/Chip';
+
 function HabitsPage() {
-  return <div>오늘의 습관</div>;
+  // { habits = [] } => // 추후 props/API 데이터로 교체 예정
+  const [habits /* setHabits*/] = useState([
+    {
+      id: 1,
+      text: '미라클 모닝 6시 기상',
+    },
+    {
+      id: 2,
+      text: '아침 챙겨 먹기',
+    },
+    {
+      id: 3,
+      text: 'React 공부 2시간',
+    },
+  ]);
+  const now = new Date();
+
+  const formattedDate = now.toLocaleString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='header-left'>
+          <h1>연우의 개발공장</h1>
+          <p>현재 시간</p>
+          <span>{formattedDate}</span>
+        </div>
+
+        <div className='header-right'>
+          <button>오늘의 집중 〉</button>
+          <button>홈 〉</button>
+        </div>
+      </div>
+
+      <div className='content'>
+        <div className='content-header'>
+          <h2>오늘의 습관</h2>
+          <button onClick={() => setIsEditMode(!isEditMode)}>
+            {isEditMode ? '완료' : '목록 수정'}
+          </button>
+        </div>
+
+        <div className='habit-list'>
+          {habits.map((habit) => (
+            <Chip key={habit.id} text={habit.text} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HabitsPage;
