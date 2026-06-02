3edafccee965ef47ccb1328299f476cb7ce2976d
HoneyLatlll <rlaalstn9415@naver.com>
2026-06-02 15:48:30 +0900

    refactor:#93 생성/수정 페이지 API 함수를 apis/study.js로 분리

--- src/pages/StudyPage/StudyCreatePage.jsx
+++ src/pages/StudyPage/StudyCreatePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import StudyForm from './components/StudyForm';
+import { createStudy } from '../../apis/study';
 export default function StudyCreatePage() {
   const [studyData, setStudyData] = useState({
     nickname: '',
@@ -9,20 +10,6 @@ export default function StudyCreatePage() {
       'https://png.pngtree.com/thumb_back/fh260/background/20241124/pngtree-celestial-circle-of-light-in-space-emitting-a-soft-glow-amidst-image_16630308.jpg',
     password: '',
   });
-  const createStudy = async (studyData) => {
-    try {
-      const res = await fetch('http://localhost:3000/studies', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ ...studyData }),
-      });
-      if (!res.ok) throw new Error('fetch 에러');
-      const data = await res.json();
-      return data;
-    } catch (err) {
-      alert(err.message);
-    }
-  };
   return (
     <>
       <StudyForm
