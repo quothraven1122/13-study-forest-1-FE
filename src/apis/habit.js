f19ce27ce01ebd8ffd60f8db44afa796a4636cc5
seonghwi-ha <digh7515@gmail.com>
2026-06-08 11:37:33 +0900

    refactor: use environment variable for API URL

--- src/apis/habit.js
+++ src/apis/habit.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getHabits = async (studyId) => {
   const response = await fetch(`${BASE_URL}/studies/${studyId}/habits`);
